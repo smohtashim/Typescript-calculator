dd4977c11048000eb240e73a146f7c5eeb48acca
Syed Mohtashim <you@example.com>
2024-03-23 01:50:39 +0500

    Push my typecript calculator to gitHub

---
+++ new.ts
@@ -0,0 +1,55 @@
+#! /usr/bin/env node
+
+import inquirer from 'inquirer';
+async function calculator() {
+    console.log('Typescript Calculator');
+    const  answer = await inquirer.prompt([
+        { message:'Enter First Number',  type:'number', name: 'firstnumber' },
+        { message: 'Enter Second Number', type: 'number', name: 'secondnumber' },
+        {
+            message: 'Select an Operator',
+            type: 'list',
+            name: 'operator',
+            choices: ['Addition " + "', 'Subtraction " - "', 'Multiplication " x "', 'Division " ÷ "'],
+        },
+
+    ]);
+
+    const { firstnumber, secondnumber, operator } = answer;
+
+    let result: number;
+    
+
+    switch (operator) {
+        case 'Addition " + "':
+            result = firstnumber + secondnumber;
+            break;
+
+        case  'Subtraction " - "':
+            result = firstnumber - secondnumber;
+            break;
+
+        case 'Multiplication " x "':
+            result = firstnumber * secondnumber;
+            break;
+            
+        case 'Division " ÷ "':
+            if (secondnumber == 0) {
+                console.log("Can't be Divided by zero");
+                return;
+            }
+
+            result = firstnumber / secondnumber;
+            break;
+         
+        default:
+            console.log('Invalid operation');
+            return; 
+
+    }
+
+    console.log(`Result is: ${result}`);
+    
+}
+
+calculator();
